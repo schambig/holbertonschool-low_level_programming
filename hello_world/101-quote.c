7f2ec4139fb0f5199090804b411633171e312515
Salomon Chambi <salomonchambi@gmail.com>
2022-06-06 11:41:19 -0700

    Fix: print a given phrase

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: 1
  */
 
 int main(void)
+/**
+ *{
+ *	fprintf(stderr
+ *		, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+ *	return (1);
+ *}
+ */
+
+/* This option is better */
 {
-	fprintf(stderr
-		, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2
+		, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
