285f4af328bccc43332e6cc20ef424e199911f9d
Salomon Chambi <salomonchambi@gmail.com>
2022-06-06 11:53:50 -0700

    Fix: print a given phrase without using puts or printf funtions

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
@@ -6,15 +5,6 @@
  */
 
 int main(void)
-/**
- *{
- *	fprintf(stderr
- *		, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
- *	return (1);
- *}
- */
-
-/* This option is better */
 {
 	write(2
 		, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
