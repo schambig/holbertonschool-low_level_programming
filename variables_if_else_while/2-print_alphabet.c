cbcc5afbf4f97c2ce548f3d75d9ad9c63dd5b816
Salomon Chambi <salomonchambi@gmail.com>
2022-06-07 10:33:46 -0700

    Print the alphabet in lowercase using putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+		putchar(10);
+
+	return (0);
+}
