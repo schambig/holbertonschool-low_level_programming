627f8d7c175b6912cc029f007de98afcf5e081c2
Salomon Chambi <salomonchambi@gmail.com>
2022-06-06 22:44:10 -0700

    Fix: tell whether a random number is positive, zero or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -20,7 +19,6 @@ int main(void)
 		printf("%i is zero\n", n);
 	else
 		printf("%i is negative\n", n);
-
 
 	return (0);
 }
