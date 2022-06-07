8817b616f3906f5f887db1f6a8893d57bc297650
Salomon Chambi <salomonchambi@gmail.com>
2022-06-07 13:21:28 -0700

    Print the lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+
+	return (0);
+}
