4beeba0c43631b31e05c4820e195b7939a2041ed
Salomon Chambi <salomonchambi@gmail.com>
2022-06-07 14:30:36 -0700

    Print digits from 0 to 9 followed by comma and space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
