aef95f91ff68a3a39225a5532c2b4ab958fef6f4
Salomon Chambi <salomonchambi@gmail.com>
2022-06-08 11:23:34 -0700

    Fix: print the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -15,20 +15,17 @@ int print_sign(int n)
 		if (n == 0)
 		{
 			_putchar('0');
-			_putchar(',');
 			return (0);
 		}
 		else
 		{
 			_putchar('-');
-			_putchar(',');
 			return (-1);
 		}
 	}
 	else
 	{
 		_putchar('+');
-		_putchar(',');
 		return (1);
 	}
 }
