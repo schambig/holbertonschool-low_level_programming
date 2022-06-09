480e18adff80f9ad34eb4aad1952f6f8eb8626e9
Salomon Chambi <salomonchambi@gmail.com>
2022-06-08 21:36:55 -0700

    Print every minute of the day, starting from 00:00 to 23:59

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day, starting from 00:00 to 23:59
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+		_putchar('0' + (h / 10));
+		_putchar('0' + (h % 10));
+		_putchar(':');
+		_putchar('0' + (m / 10));
+		_putchar('0' + (m % 10));
+		_putchar('\n');
+		}
+	}
+}
