f05767b8d2e273fd6b4125df11af28e89b3c6c71
Salomon Chambi <salomonchambi@gmail.com>
2022-06-09 20:05:59 -0700

    Print all natural numbers from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,13 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		printf("%d, ", i);
+			if (i == 98)
+			{
+				putchar('9');
+				putchar('8');
+			}
 	}
 	else if (n == 98)
 	{
@@ -20,8 +25,13 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		printf("%d, ", i);
+			if (i == 98)
+			{
+				putchar('9');
+				putchar('8');
+			}
 	}
 	putchar('\n');
 }
