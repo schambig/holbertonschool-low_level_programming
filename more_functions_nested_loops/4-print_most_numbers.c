053a338e275b532884ab4bf556bc1927bdf5f61c
Salomon Chambi <salomonchambi@gmail.com>
2022-06-13 09:40:50 -0700

    Print numbers from 0 to 9, except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints the numbers, from 0 to 9, except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		_putchar(i);
+	}
+	_putchar('\n');
+}
