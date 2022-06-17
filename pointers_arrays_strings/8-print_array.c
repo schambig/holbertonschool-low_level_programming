62eae72263fc779c28ab252199545e8cf2dcd5d5
Salomon Chambi <salomonchambi@gmail.com>
2022-06-16 22:31:14 -0700

    Fix: print n elements of an array of integers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,10 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", *(a + i));
-	printf("%d\n", *(a + n - 1));
+	if (n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", *(a + i));
+		printf("%d\n", *(a + n - 1));
+	}
 }
