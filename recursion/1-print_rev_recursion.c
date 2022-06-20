afe66f5c4f01adde48e1c0389fef7191932f7a2a
Salomon Chambi <salomonchambi@gmail.com>
2022-06-20 15:04:17 -0700

    Prints a string in reverse recursively

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to print recursively
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
