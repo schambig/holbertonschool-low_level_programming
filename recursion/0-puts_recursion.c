a6ffb6d8919925ec2f0596c9e5daaf2973c2ac00
Salomon Chambi <salomonchambi@gmail.com>
2022-06-20 14:22:02 -0700

    Fix: print a string recursively, followed by a new line

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,13 +7,15 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		_puts_recursion(&s[i + 1]);
+		_putchar('\n');
+		return;
 	}
 	else
-		 _putchar('\n');
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s++);
+	}
 }
