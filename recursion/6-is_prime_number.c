de49db71685240deb91dbd308b5b7932ff5d1824
Salomon Chambi <salomonchambi@gmail.com>
2022-06-20 22:30:41 -0700

    Fix: check if an integer is prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,32 +2,32 @@
 /**
  * is_prime_number - Check if the integer is prime
  * @n: The number to check
- * @p: The posible prime
+ * @p: The possible prime
  * Return: 1 if the input integer is a prime number, otherwise return 0
  */
 
 int get_prime(int n, int p);
 int is_prime_number(int n)
 {
-	if (n <= 1) /*three first cases*/
+	if (n <= 1) /*Three first cases*/
 		return (0);
 	else
-		return (get_prime(n, 2));
+		return (get_prime(n, 2)); /*Calls the rec. func. starting from 2*/
 }
 
 /**
  * get_prime - Auxiliar recursive function to find the prime
  * @n: The number to check
- * @p: The posible prime
+ * @p: The possible prime
  * Return: 1 if the input integer is a prime number, otherwise return 0
  */
 
 int get_prime(int n, int p)
 {
-	if (n == p)
+	if (n == p) /*Case for number 2*/
 		return (1);
-	else if (n % p == 0)
+	else if (n % p == 0) /*Checks if n is divisible by p*/
 		return (0);
 	else
-		return (get_prime(n, p + 1));
+		return (get_prime(n, p + 1)); /*Increment p*/
 }
