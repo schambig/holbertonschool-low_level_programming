35ec5d7318136ba080eed35e10f48304e695fa29
Salomon Chambi <salomonchambi@gmail.com>
2022-06-22 12:21:27 -0700

    Multiply two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 {
 	int i, prod = 1;
 
-	if (argc <= 1)
+	if (argc <= 2)
 		printf("Error\n");
 	else
 	{
 		for (i = 1; i < argc; i++)
-			prod = prod * atoi(argv[i]);
+			prod *= atoi(argv[i]);
 		printf("%d\n", prod);
 	}
 	return (0);
