94b45178a413d8c47b91a88ef0ef491197e16bfd
Salomon Chambi <salomonchambi@gmail.com>
2022-06-22 10:42:59 -0700

    Fix: print its own name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * whats_my_name - Prints its own name
+ * main - Prints its own name
  * @argc: The size of the argv array
  * @argv: An array of size argc
  * Return: Always 0 (Success)
