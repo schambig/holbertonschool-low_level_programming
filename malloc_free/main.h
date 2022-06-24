8b7915e727e969f01c1a64dddd6c8989ab41bfa4
Salomon Chambi <salomonchambi@gmail.com>
2022-06-23 18:30:57 -0700

    keep the prototypes of the project

---
+++ malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_MALLOC_FREE
+#define HEADER_MALLOC_FREE
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+#endif /*HEADER_MALLOC_FREE*/
