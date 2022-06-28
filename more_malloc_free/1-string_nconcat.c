d01de2e3171d0220ea3f29123ba03dc9f68e15af
Salomon Chambi <salomonchambi@gmail.com>
2022-06-27 18:45:01 -0700

    Fix: concatenate two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,33 +26,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s = malloc(sizeof(char) * (len1 + n + 1));
 		if (s == NULL)
-		{
 			return (NULL);
-		}
 		for (i = 0; i < len1; i++)
-		{
 			s[i] = s1[i];
-		}
 		for  (i = 0; i < n; i++)
-		{
 			s[len1 + i] = s2[i];
-		}
 		s[len1 + n] = '\0';
 		return (s);
 	}
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < len1; i++)
-	{
 		s[i] = s1[i];
-	}
 	for  (i = 0; i < len2; i++)
-	{
 		s[len1 + i] = s2[i];
-	}
 	s[len1 + len2] = '\0';
 	return (s);
 }
