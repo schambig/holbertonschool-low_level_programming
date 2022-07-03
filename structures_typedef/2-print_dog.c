143e08db79a9323a224f924e648ca83e18111369
Salomon Chambi <salomonchambi@gmail.com>
2022-07-02 22:23:17 -0700

    Fix: print a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,8 +10,16 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name);
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
