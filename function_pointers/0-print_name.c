10d6bb94e9a4de8efea8170635c7380e2c2129e9
Salomon Chambi <salomonchambi@gmail.com>
2022-07-03 17:45:28 -0700

    Print a name using a function pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name using a function pointer
+ * @name: The name to print
+ * @f: Pointer to a function (callback)
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+/* f is a callback function to be called by print_name */
+{
+	f(name);
+}
