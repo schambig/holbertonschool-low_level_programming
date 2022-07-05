53a904deb87c6a9236383ae45415bfe68e6b0821
Salomon Chambi <salomonchambi@gmail.com>
2022-07-04 19:20:04 -0700

    Fix: print strings through a variadic function

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,9 +18,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(ap, char *);
-		printf("%s", s == NULL ? "(nil)" : s);
-		if (i < n - 1 && separator != NULL)
-			printf("%s", separator);
+		printf("%s", (s == NULL) ? "(nil)" : s);
+		printf("%s", (separator && i < n - 1) ? separator : "");
 	}
 	va_end(ap);
 	printf("\n");
