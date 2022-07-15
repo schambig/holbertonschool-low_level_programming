590b12d77294af0547a467d96ce2a2fe3774a396
Salomon Chambi <salomonchambi@gmail.com>
2022-07-14 23:59:37 -0700

    Contain the project struc and prototypes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define LINKED_LISTS_H
 
 #include <stdlib.h>
- #include <stdio.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -14,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
