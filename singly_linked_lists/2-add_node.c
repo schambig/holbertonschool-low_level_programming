6d7c1c4058e6da4f48cb3a7376c9c7f2c55fff19
Salomon Chambi <salomonchambi@gmail.com>
2022-07-15 20:35:32 -0700

    Add a new node at the beginning of a singly linked list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,27 +10,27 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = NULL;
 
-	/* verifying if the argument passed exist */
+	/* verify if the argument passed exist */
 	if (head == NULL)
 		return (NULL);
 
-	/* dynamically allocating memory for the new node */
+	/* dynamically allocate memory for new_node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* assigning values to the new node */
+	/* assign values to new_node */
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 
-	/* verifying if the list exist (is no empty) */
+	/* verify if the list exist (is no empty) */
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	/* making the new node the head of the list */
+	/* insert new_node at the beginning and make it the head */
 	new_node->next = *head;
 	*head = new_node;
 
