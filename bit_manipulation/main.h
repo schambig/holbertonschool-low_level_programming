593b51762d02ea2aa59c2e4fcdecc343ce5fa4cb
Salomon Chambi <salomonchambi@gmail.com>
2022-07-19 08:19:55 -0700

    Contain the project struc and prototypes

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef BIT_MANIPULATION_H
 #define BIT_MANIPULATION_H
 
+#include <string.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
