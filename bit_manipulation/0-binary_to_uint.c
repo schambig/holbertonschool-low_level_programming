141e22132b0ac5b1870af226e496be9edec9efcf
Salomon Chambi <salomonchambi@gmail.com>
2022-07-19 08:26:24 -0700

    Fix: convert a binary number to an unsigned int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,14 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len = strlen(b);
+	int len = 0;
 	int i = 0;
 	int total = 0, pow2 = 1;
 
 	if (b == NULL)
 		return (0);
 
+	len = strlen(b);
 	for (i = (len - 1); i >= 0; i--)
 	{
 		if (b[i] == '0' || b[i] == '1')
