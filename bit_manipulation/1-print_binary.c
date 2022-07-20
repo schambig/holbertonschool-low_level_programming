9aa5133e157b3c34829e3e584509aeada61d6083
Salomon Chambi <salomonchambi@gmail.com>
2022-07-19 18:17:12 -0700

    Fix: print the binary representation of a number

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -5,39 +5,18 @@
  * @n: The number to parse
  * Return: Nothing
  */
-int binary_digit_used(unsigned int n);
-
 void print_binary(unsigned long int n)
 {
-	int bindig = binary_digit_used(n);
-	int i = 0, j;
+	unsigned int digit = 0, j = 0;
+	/* copy the value of n in a new variable to not modify it */
+	unsigned long int num = n;
 
-	if (n == 0)
-		_putchar('0');
+	/* get the number of digits used to express n in binary */
+	for (digit = 0; num; digit++)
+		num = num >> 1;
 
-	for (i = bindig - 1; i >= 0; i--)
-	{
-		j = n >> i;
-		if (j & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
-}
-
-/**
- * binary_digit_used - Get the number of binary digits used
- * @n: The number to parse
- * Return: The number of binary digits
- */
-int binary_digit_used(unsigned int n)
-{
-	int count = 0;
-
-	while (n != 0)
-	{
-		n *= 0.5;
-		count++;
-	}
-	return (count);
+	/* do logical product '&' and print 1 or 0 */
+	for (j = digit - 1; j > 0 && n; j--)
+		_putchar(((n >> j) & 1) + '0');
+	_putchar(((n >> j) & 1) + '0');
 }
