e8536472709d106a2f2136fa6f9756c80c365f9a
Salomon Chambi <salomonchambi@gmail.com>
2022-08-19 23:40:31 -0700

    Fix: allocate memory for each element of the new hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,17 +4,31 @@
  * hash_table_create - Create a hash table
  * @size: Size of the array
  * Return: Returns a pointer to the newly created hash table,
- *	   If something went wrong, your function should return NULL
+ *	   If something went wrong, return NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
+	(void)size;
 
-	ht = malloc(sizeof(hash_table_t *) * size);
+	/* allocate memory for ht which points to a new hash table (matrix) */
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
+
+	/* set size of array, where each element points to the first node of a SLL */
+	ht->size = size;
+	/* allocate memory for each element (pointer) of the array itself */
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht->array);
+		free(ht);
+		return (NULL);
+	}
+
 	return (ht);
 }
