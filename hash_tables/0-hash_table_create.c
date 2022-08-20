0a4e09fa6b4f7c4da2b6623d1ec1aa733d6eb4b3
Salomon Chambi <salomonchambi@gmail.com>
2022-08-19 22:13:10 -0700

    Fix: correct data type declaration and memory block amount

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,11 +6,11 @@
  * Return: Returns a pointer to the newly created hash table,
  *	   If something went wrong, your function should return NULL
  */
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	char *ht = NULL;
+	hash_table_t *ht = NULL;
 
-	ht = malloc(sizeof(size));
+	ht = malloc(sizeof(hash_table_t *) * size);
 	if (ht == NULL)
 	{
 		free(ht);
