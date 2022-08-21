c03d7b1f6e74cc55edbb7922c8d5500e4745910c
Salomon Chambi <salomonchambi@gmail.com>
2022-08-21 00:51:50 -0700

    Fix: receive a hash value and map it into an index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -13,7 +13,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	 * The modulo operator is far from ideal, but it is good enough.
 	 * It guarantees that the resulting hash bucket is in range:
 	 * the result of key % num_buckets is always in range of 0..(num_buckets-1).
-	 * 
+	 *
 	 * Source (Quora): https://bit.ly/3ClU1zj
 	 */
 	return (hash_djb2(key) % size);
