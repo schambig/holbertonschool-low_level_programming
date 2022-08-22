a83c4dcbc395e47c453e7cd28c7a01203fb91841
Salomon Chambi <salomonchambi@gmail.com>
2022-08-21 19:53:37 -0700

    Delete a hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Delete a hash table
+ * @ht: Hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_table_t *head = ht;
+	hash_node_t *node, *temp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
