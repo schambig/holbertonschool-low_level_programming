bf2556d201a435cd9c7708e991a77b102dcd8632
Salomon Chambi <salomonchambi@gmail.com>
2022-08-25 15:30:39 -0700

    Contain the prototypes of the project

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCHING_ALGORITHMS_H
+#define SEARCHING_ALGORITHMS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* prototypes of the proyect */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCHING_ALGORITHMS_H */
