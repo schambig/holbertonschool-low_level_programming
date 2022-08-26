7ce2b03d0d95f1b8fccaf459b33967475083f8e9
Salomon Chambi <salomonchambi@gmail.com>
2022-08-25 23:26:47 -0700

    Fix: search for a value in a sorted array of ints (binary search)

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,45 +1,6 @@
 #include "search_algos.h"
 
 /**
- * real_binary_search - Search for a target in a sorted array of integers
- * @array: Array to search in
- * @left: Left element
- * @right: Right element
- * @target: Value to search for
- * Return: Index where value is located,
- *	   If value is not present in array or if array is NULL, return -1
- */
-int real_binary_search(int *array, int left, int right, int target)
-{
-	int mid = 0, i;
-
-	if (array == NULL)
-		return (-1);
-
-	while (left <= right)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-
-
-		mid = (left + right) / 2;
-
-		if (array[mid] == target)
-			return (mid);
-
-		if (array[mid] < target)
-			left = mid + 1;
-
-		else
-			right = mid - 1;
-	}
-
-	return (-1);
-}
-
-/**
  * binary_search - Search for a value in a sorted array of integers
  * @array: Array to search in
  * @size: Size of the array
@@ -49,5 +10,24 @@ int real_binary_search(int *array, int left, int right, int target)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	return (real_binary_search(array, array[0], array[size - 1], value));
+	int i, left, right;
+
+	if (!array)
+		return (-1);
+	for (left = 0, right = (int)size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+	return (-1);
 }
